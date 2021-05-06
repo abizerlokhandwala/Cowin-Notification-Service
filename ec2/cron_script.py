ef4bef395c68b27780d44a2bccc3f6589f2b8336
Abizer Lokhandwala <abizer.lokhandwala@codenation.co.in>
2021-05-06 05:51:39 +0530

    ec2 change to lambda invoke

--- ec2/cron_script.py
+++ ec2/cron_script.py
@@ -1,10 +1,15 @@
+import os
 import time
+import boto3
+import os
 
-import requests
-
-TRIGGER_URL = 'https://a7nn6pz85i.execute-api.ap-south-1.amazonaws.com/dev/trigger'
+client = boto3.client('lambda', region_name='ap-south-1',
+                      aws_access_key_id=os.getenv('aws_access_key'),
+                      aws_secret_access_key=os.getenv('aws_secret_key')
+                      )
 
 if __name__ == "__main__":
     while True:
-        response = requests.post(TRIGGER_URL)
+        response = client.invoke(FunctionName="cowin-notification-service-dev-trigger_district_updates",
+                      InvocationType='Event',Payload='')
         time.sleep(20)
