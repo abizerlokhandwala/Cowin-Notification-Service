273e45509390efa54c2f1ae115ffc6d88b043cb0
ashawe <harshsaglani@gmail.com>
2021-05-12 10:32:51 +0530

    pause cron between 0000 & 0530

--- ec2/cron_script.py
+++ ec2/cron_script.py
@@ -4,6 +4,7 @@ import string
 import time
 import boto3
 import os
+from datetime import datetime, time
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -19,15 +20,26 @@ def random_str(length):
     res = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
     return str(res)
 
+# Checking if time is between a specific range
+# https://stackoverflow.com/questions/10048249/how-do-i-determine-if-current-time-is-within-a-specified-range-using-pythons-da
+def is_time_between(begin_time, end_time, check_time=None):
+    # If check time is not given, default to current UTC time
+    check_time = check_time or datetime.utcnow().time()
+    if begin_time < end_time:
+        return check_time >= begin_time and check_time <= end_time
+    else: # crosses midnight
+        return check_time >= begin_time or check_time <= end_time
+
 if __name__ == "__main__":
     while True:
         try:
-            response = client.invoke(FunctionName=TRIGGER_FUNCTION_NAME,
-                          InvocationType='Event',Payload='')
-            var = client.get_function_configuration(FunctionName=UPDATE_FUNCTION_NAME)
-            var['Environment']['Variables']['DUMMY'] = random_str(20)
-            client.update_function_configuration(FunctionName=UPDATE_FUNCTION_NAME,
-                                                 Environment=var['Environment'])
+            if not is_time_between( time(00,00) , time(5,30) ):
+                response = client.invoke(FunctionName=TRIGGER_FUNCTION_NAME,
+                            InvocationType='Event',Payload='')
+                var = client.get_function_configuration(FunctionName=UPDATE_FUNCTION_NAME)
+                var['Environment']['Variables']['DUMMY'] = random_str(20)
+                client.update_function_configuration(FunctionName=UPDATE_FUNCTION_NAME,
+                                                    Environment=var['Environment'])
             time.sleep(10)
         except Exception as e:
             print(e)
